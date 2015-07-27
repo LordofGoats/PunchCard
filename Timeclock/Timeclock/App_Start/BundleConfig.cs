45d960a6d3d23778de254b2a6f76dc5e1530392c
Hugh Jones <hughjones@libreworx.com>
2015-07-27 13:39:16 -0400

    jQuery not loaded

--- Timeclock/Timeclock/App_Start/BundleConfig.cs
+++ Timeclock/Timeclock/App_Start/BundleConfig.cs
@@ -9,7 +9,7 @@ namespace Timeclock
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-1.8.2.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));

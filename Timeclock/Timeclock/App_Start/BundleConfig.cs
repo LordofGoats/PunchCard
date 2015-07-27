e90e960433dcb02302d7090cff5bd288c1fb4406
Carson Beam <spreadthelife@gmail.com>
2015-07-27 16:25:42 -0400

    Css fixed for now. I still don't know what happened.

--- Timeclock/Timeclock/App_Start/BundleConfig.cs
+++ Timeclock/Timeclock/App_Start/BundleConfig.cs
@@ -26,7 +26,6 @@ namespace Timeclock
                       "~/Scripts/jquery.cookie.js",
                       "~/Scripts/jquery-ui-1.9.2.custom.min.js",
                       "~/Scripts/slimScroll.min.js",
-                      //"~/Scripts/jquery-1.8.2.min.js",
                       "~/Scripts/fullcalendar.min.js",
                       "~/Scripts/jquery.uniform.min.js",
                       "~/Scripts/chosen.jquery.min.js",
@@ -34,33 +33,20 @@ namespace Timeclock
                       "~/Scripts/app.js"
                 ));
 
-            bundles.Add(new ScriptBundle("~/bundles/calendar").Include(
-                    //"~/Scripts/fullcalendar.min.js",
-                    //"~/Scripts/jquery.uniform.min.js",
-                    //"~/Scripts/chosen.jquery.min.js",
-                    //"~/Scripts/jquery.fancybox.pack.js",
-                    //"~/Scripts/app.js"
-                ));
-
             bundles.Add(new StyleBundle("~/Content/css").Include(
+                    //"~/Content/bootstrap.css",
                     "~/Content/bootstrap.min.css",
-                    "~/Content/bootstrap-responsive.css",
-                    "~/Content/bootstrap-fullcalendar.css",
-                    "~/Content/bootstrap.css",
-                    //"~/Content/site.css",
+                    "~/Content/bootstrap-responsive.min.css",
+                    "~/Content/font-awesome.min.css",
                     "~/Content/style.css",
                     "~/Content/style_responsive.css",
                     "~/Content/style_default.css",
-                    "~/Content/chosen.css",
-                    "~/Content/uniform.default.css",
-                    "~/Content/jquery.fancybox.css"                      
-                      //"~/Content/bootstrap-responsive.min.css",
-                      //"~/Content/bootstrap-fileupload.css",
+                    "~/Content/jquery.fancybox.css",
+                    //"~/Content/chosen.css",
+                    "~/Content/uniform.default.css",                      
+                    //"~/Content/bootstrap-fileupload.css",
+                    "~/Content/bootstrap-fullcalendar.css"
                       ));
-
-            //bundles.Add(new StyleBundle("~/Content/Calendar/css").Include(
-                    
-            //    ));
         }
     }
 }
